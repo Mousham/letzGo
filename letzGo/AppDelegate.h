f7234f744040c3fa230a7340ca9675507a1b7960
Mausam <mac@macs-macbook-pro.local>
2018-10-08 10:47:43 +0545

    Initial Commit

---
+++ letzGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  letzGo
+//
+//  Created by mac on 10/8/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ letzGo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  letzGo
+//
+//  Created by mac on 10/8/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
